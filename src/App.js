a54ca8e2b7599fdb1319b384d131ee13d0d06c9e
Lorenzo Quiambao <lorenzotquiambao@gmail.com>
2023-02-17 12:23:15 -0500

    Added graphql and storage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,134 @@
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
+import { API, Storage } from "aws-amplify";
 import {
-  withAuthenticator,
   Button,
+  Flex,
   Heading,
   Image,
+  Text,
+  TextField,
   View,
-  Card,
+  withAuthenticator,
 } from "@aws-amplify/ui-react";
+import { listTodos } from "./graphql/queries";
+import {
+  createTodo as createTodoMutation,
+  deleteTodo as deleteTodoMutation,
+} from "./graphql/mutations";
 
-function App({ signOut }) {
+const App = ({ signOut }) => {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  async function fetchTodos() {
+    const apiData = await API.graphql({ query: listTodos });
+    const todosFromAPI = apiData.data.listTodos.items;
+    await Promise.all(
+      todosFromAPI.map(async (todo) => {
+        if (todo.image) {
+          const url = await Storage.get(todo.name);
+          todo.image = url;
+        }
+        return todo;
+      })
+    );
+    setTodos(todosFromAPI);
+  }
+
+  async function createTodo(event) {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    const image = form.get("image");
+    const data = {
+      name: form.get("name"),
+      description: form.get("description"),
+      image: image.name,
+    };
+    if (!!data.image) await Storage.put(data.name, image);
+    await API.graphql({
+      query: createTodoMutation,
+      variables: { input: data },
+    });
+    fetchTodos();
+    event.target.reset();
+  }
+
+  async function deleteTodo({ id, name }) {
+    const newtodos = todos.filter((note) => note.id !== id);
+    setTodos(newtodos);
+    await Storage.remove(name);
+    await API.graphql({
+      query: deleteTodoMutation,
+      variables: { input: { id } },
+    });
+  }
+
   return (
     <View className="App">
-      <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
+      <Heading level={1}>My todos App</Heading>
+      <View as="form" margin="3rem 0" onSubmit={createTodo}>
+        <Flex direction="row" justifyContent="center">
+          <TextField
+            name="name"
+            placeholder="Note Name"
+            label="Note Name"
+            labelHidden
+            variation="quiet"
+            required
+          />
+          <TextField
+            name="description"
+            placeholder="Note Description"
+            label="Note Description"
+            labelHidden
+            variation="quiet"
+            required
+          />
+          <View
+            name="image"
+            as="input"
+            type="file"
+            style={{ alignSelf: "end" }}
+          />
+          <Button type="submit" variation="primary">
+            Create Note
+          </Button>
+        </Flex>
+      </View>
+      <Heading level={2}>Current todos</Heading>
+      <View margin="3rem 0">
+        {todos.map((todo) => (
+          <Flex
+            key={todo.id || todo.name}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Text as="strong" fontWeight={700}>
+              {todo.name}
+            </Text>
+            <Text as="span">{todo.description}</Text>
+            {todo.image && (
+              <Image
+                src={todo.image}
+                alt={`visual aid for ${todo.name}`}
+                style={{ width: 400 }}
+              />
+            )}
+            <Button variation="link" onClick={() => deleteTodo(todo)}>
+              Delete todo
+            </Button>
+          </Flex>
+        ))}
+      </View>
       <Button onClick={signOut}>Sign Out</Button>
     </View>
   );
-}
+};
 
 export default withAuthenticator(App);
